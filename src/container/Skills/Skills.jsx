3b4847a7e4521e824c89a36c4ed1b93fa51b3890
dmshikder <dmshikder@gmail.com>
2022-08-25 15:15:01 +0600

    skills section added

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
-import './Skills';
+import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { client, urlFor } from "../../client";
+import "./Skills.scss";
+
+
+
 
 const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperience(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
   return (
-    <div>
-      <h2>skills</h2>
+    <>
+    <h2 className="head-text"> Skills </h2>
+
+    <div className="app__skills-container">
+      <motion.div>
+        {
+          skills.map((skill)=>(
+            <motion.div
+            whileInView={{opacity:[0,1]}}
+            transition={{duration:0.5}}
+            className='app__skills-item app__flex'
+            key={skill.name}
+            >
+              <div className="app__flex" style={{backgroundColor:skill.bgcolor}}>
+                <img src={urlFor(skill.icon)} alt="skill.name" />
+
+              </div>
+              <p className="p-text">{skill.name}</p>
+
+            </motion.div>
+
+          ))
+        }
+
+
+      </motion.div>
+
+      <motion.div className="app__">
+
+
+      </motion.div>
+
+
     </div>
+    </>
   );
 };
 
